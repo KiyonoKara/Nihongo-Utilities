893d5ca8b2ed048e394e18b11bda6bf73f58224e
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-09-05 07:56:32 -0700

    Fix: Fixed the regex and tested it.

--- src/日本語.java
+++ src/日本語.java
@@ -15,13 +15,13 @@ import java.util.regex.Matcher;
 @SuppressWarnings("NonAsciiCharacters")
 public class 日本語 {
     // TODO: これを直せる
-    static String regexの約物 = "/[\\u3000-\\u303F]/g",
-           regexの平仮名 = "\"[\\u3040-\\u309f]\"g",
-           regexの片仮名 = "/[\\u30a0-\\u30ff]/g",
-           regexのローマ字 = "/[\\uff00-\\uff9f]/g",
-           regexのCJK = "/[\\u4e00-\\u9faf]/g",
-           regexの拡張CJK = "/[\\u3400-\\u4dbf]]/g",
-           regexの全部 = "/[\\u3000-\\u303F]|[\\u3040-\\u309F]|[\\u30A0-\\u30FF]|[\\uFF00-\\uFFEF]|[\\u4E00-\\u9FAF]|[\\u2605-\\u2606]|[\\u2190-\\u2195]|\\u203B|[0-9]/g";
+    static String regexの約物 = "[\u3000-\u303F]",
+           regexの平仮名 = "[\u3040-\u309f]",
+           regexの片仮名 = "[\u30a0-\u30ff]",
+           regexのローマ字 = "[\uff00-\uff9f]",
+           regexのCJK = "[\u4e00-\u9faf]",
+           regexの拡張CJK = "[\u3400-\u4dbf]]",
+           regexの全部 = "[\u3000-\u303F]|[\u3040-\u309F]|[\u30A0-\u30FF]|[\uFF00-\uFFEF]|[\u4E00-\u9FAF]|[\u2605-\u2606]|[\u2190-\u2195]|\u203B|[0-9]";
 
     /**
      * 盛れる（もれる）
