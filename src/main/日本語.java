8c8cae84b681571eb38182ca4ad3c0fc7d4c9eaf
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2021-09-22 08:57:16 -0700

    Documentation: Add unicode documentation.

--- src/main/日本語.java
+++ src/main/日本語.java
@@ -27,12 +27,12 @@ public class 日本語 {
            正規表現の全部 = "[\u3000-\u303F]|[\u3040-\u309F]|[\u30A0-\u30FF]|[\uFF00-\uFFEF]|[\u4E00-\u9FAF]|[\u2605-\u2606]|[\u2190-\u2195]|\u203B|[0-9]";
 
     // Zenkaku Hiragana Bounds
-    // First Hiragana character
+    // First Hiragana character, U+3041
     // 最初の全角平仮名の文字
     private static final char firstZenkakuHiragana = 'ぁ';
 
-    // Last Hiragana character
-    // 最後の全角平仮名の文字
+    // Last Hiragana character,
+    // 最後の全角平仮名の文字, U+3096
     private static final char lastZenkakuHiragana = 'ゖ';
 
     // Zenkaku Katakana Bounds
