d4bdce229eaa64ab97288ae90dd30f13ef2cef09
Yen <68307546+KaNguy@users.noreply.github.com>
2021-10-12 09:34:52 -0700

    Feature: Add regex constants.

--- src/main/Nihongo.java
+++ src/main/Nihongo.java
@@ -18,5 +18,12 @@ import java.util.HashMap;
 import java.text.Normalizer;
 
 public class Nihongo {
-
+    static String
+                regexPunctuation = "[\u3000-\u303F]",
+                regexHiragana = "[\u3040-\u309f]",
+                regexKatakana = "[\u30a0-\u30ff]",
+                regexRomaji = "[\uff00-\uff9f]",
+                regexCJK = "[\u4e00-\u9faf]",
+                regexExtendedCJK = "[\u3400-\u4dbf]]",
+                regexAll = "[\u3000-\u303F]|[\u3040-\u309F]|[\u30A0-\u30FF]|[\uFF00-\uFFEF]|[\u4E00-\u9FAF]|[\u2605-\u2606]|[\u2190-\u2195]|\u203B|[0-9]";
 }
