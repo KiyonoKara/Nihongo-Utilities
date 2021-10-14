ffbd45bc32089b9a5f6a1fec07ce9683163e242a
Yen <68307546+KaNguy@users.noreply.github.com>
2021-10-14 08:16:31 -0700

    Feature: Add bounds.

--- src/main/Nihongo.java
+++ src/main/Nihongo.java
@@ -26,4 +26,25 @@ public class Nihongo {
                 regexCJK = "[\u4e00-\u9faf]",
                 regexExtendedCJK = "[\u3400-\u4dbf]]",
                 regexAll = "[\u3000-\u303F]|[\u3040-\u309F]|[\u30A0-\u30FF]|[\uFF00-\uFFEF]|[\u4E00-\u9FAF]|[\u2605-\u2606]|[\u2190-\u2195]|\u203B|[0-9]";
+
+    // Zenkaku Hiragana Bounds
+    // First Hiragana character, U+3041
+    private static final char firstZenkakuHiragana = 'ぁ';
+
+    // Last Hiragana character, U+3096
+    private static final char lastZenkakuHiragana = 'ゖ';
+
+    // Zenkaku Katakana Bounds
+    // First Full-Width Katakana character, U+30A1
+    private static final char firstZenkakuKatakana = 'ァ';
+
+    // Last Full-Width Katakana character, U+30FA
+    private static final char lastZenkakuKatakana = 'ヺ';
+
+    // Hankaku Katakana Bounds
+    // First Half-Width Katakana character, U+FF66
+    private static final char firstHankakuKatakana = 'ｦ';
+
+    // Last Half-Width Katakana character, U+FF9D
+    private static final char lastHankakuKatakana = 'ﾝ';
 }
