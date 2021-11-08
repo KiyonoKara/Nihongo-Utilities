703d421378304b4f0fedcdd7d3e43c41ac50f5fa
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2021-11-08 08:34:20 -0800

    Tests: Add EN tests.

---
+++ src/test/TestEN.java
@@ -0,0 +1,9 @@
+package test;
+
+import main.Nihongo;
+
+public class TestEN {
+    public static void main(String[] args) {
+        System.out.println(Nihongo.convertKatakanaToRomaji("キョキュキャ"));
+    }
+}
