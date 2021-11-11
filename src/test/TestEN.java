e7578b5d0b43898119b961ccb6dd836069480309
Yen <68307546+KaNguy@users.noreply.github.com>
2021-11-11 07:09:24 -0800

    Tests: Add more tests.

--- src/test/TestEN.java
+++ src/test/TestEN.java
@@ -5,5 +5,7 @@ import main.Nihongo;
 public class TestEN {
     public static void main(String[] args) {
         System.out.println(Nihongo.convertKatakanaToRomaji("キョキュキャ"));
+        System.out.println(Nihongo.toHiragana("はい、テスト"));
+        System.out.println(Nihongo.toKatakana("カタカナ、ひらがな"));
     }
 }
